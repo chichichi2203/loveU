bd6ba9dda3fb03f0a77c418790679b89040e5e14
chichichi2203 <chichichi2203@gmail.com>
2021-11-23 18:57:58 +0700

    iloveyou

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    titleWeb: "I Love You",
+    introTitle: 'Hello anh',
+    introDesc: `Ngay hom nay cua anh the nao? 
+    Ban ron, ap luc hay la vui ve binh yen`,
+    btnIntro: '^^Du la the nao cung nhan vao day nhe^^',
+    title: 'Toc em mau den Moi em mau do' ,
+    desc: 'Lai em hoi nho, anh co nho em khong? ',
+    btnYes: 'Nho em',
+    btnNo: 'Khong nho em',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao anh lai yeu em nhi',
+    btnReply: 'Gui em',
+    reply: 'Vi anh biet em cung yeu anh',
+    mess: 'Yeu anh',
+    messDesc: 'Em da tung muon chinh phuc ca the gioi, gap anh roi moi chot nhan ra, tung manh, tung manh trong the gioi ay deu la anh',
+    btnAccept: 'Dong y thi bam vao day',
+    messLink: 'https://www.facebook.com/messages/t/100029383322998/'
+}
